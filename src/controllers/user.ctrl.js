f23d974a653fc87719d1ce93267ced482c279ca5
Arpit Chokniwal <arpit@expertia.ai>
2023-03-18 11:20:07 +0530

    update user

--- src/controllers/user.ctrl.js
+++ src/controllers/user.ctrl.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const User = require("../schema/user.schema");
 
-router.get("/:user_id", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const user_id = req.params.user_id;
-    if (req.user._id != user_id)
-      return res.status(400).send({ message: "Invalid user id" });
-    if (!user_id) return res.status(400).send({ message: "User Id required!" });
+    const user_id = req.user._id;
+    // if (req.user._id != user_id)
+    //   return res.status(400).send({ message: "Invalid user id" });
+    if (!user_id) return res.status(400).send({ message: "Token required!" });
     const user = await User.findById({ _id: user_id });
-    if (!user) return res.status(400).send({ message: "Invalid user id!" });
+    if (!user) return res.status(400).send({ message: "Invalid Token!" });
     res.status(200).send({ status: true, user });
   } catch (error) {
     return res.status(500).send(error);
@@ -17,16 +17,16 @@ router.get("/:user_id", async (req, res) => {
 
 router.patch("/:user_id", async (req, res) => {
   try {
-    const user_id = req.params.user_id;
-    if (req.user._id != user_id)
-      return res.status(400).send({ message: "Invalid user id" });
+    const user_id = req.user._id;
+    // if (req.user._id != user_id)
+    //   return res.status(400).send({ message: "Invalid user id" });
     const body = req.body;
     if (!user_id || !body)
-      return res.status(400).send({ message: "User Id or Body required!" });
+      return res.status(400).send({ message: "Token or Body required!" });
     const user = await User.findByIdAndUpdate({ _id: user_id }, body, {
       new: true,
     });
-    if (!user) return res.status(400).send({ message: "Invalid user id!" });
+    if (!user) return res.status(400).send({ message: "Invalid Token!" });
     res.status(200).send({ status: true, user });
   } catch (error) {
     return res.status(500).send(error);
