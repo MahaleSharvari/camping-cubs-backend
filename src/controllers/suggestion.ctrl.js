b0616eebdb7c6d0c2cc626f9b98d26a94cd3dbd2
Arpit Chokniwal <arpit@expertia.ai>
2023-04-03 22:53:05 +0530

    suggestion api added

--- src/controllers/suggestion.ctrl.js
+++ src/controllers/suggestion.ctrl.js
@@ -4,7 +4,45 @@ const Campground = require("../schema/campground.schema");
 router.post("/", async (req, res) => {
   try {
     const { latitude, longitude, price } = req.body;
-
+    let allStates = [
+      "Andaman and Nicobar Islands",
+      "Andhra Pradesh",
+      "Arunāchal Pradesh",
+      "Assam",
+      "Bihār",
+      "Chandīgarh",
+      "Chhattīsgarh",
+      "Damān and Diu",
+      "Delhi",
+      "Dādra and Nagar Haveli",
+      "Goa",
+      "Gujarāt",
+      "Haryāna",
+      "Himāchal Pradesh",
+      "Jammu and Kashmīr",
+      "Jharkhand",
+      "Karnātaka",
+      "Kerala",
+      "Lakshadweep",
+      "Madhya Pradesh",
+      "Mahārāshtra",
+      "Manipur",
+      "Meghālaya",
+      "Mizoram",
+      "Nāgāland",
+      "Odisha",
+      "Puducherry",
+      "Punjab",
+      "Rājasthān",
+      "Sikkim",
+      "Tamil Nādu",
+      "Telangana",
+      "Tripura",
+      "Uttar Pradesh",
+      "Uttarakhand",
+      "West Bengal",
+    ];
+    allStates.sort(() => Math.random() - 0.5);
     if (!latitude || !longitude) {
       return res.status(400).send({ message: "Missing latitude or longitude" });
     }
@@ -21,34 +59,38 @@ router.post("/", async (req, res) => {
       },
       { $sort: { distance: 1 } },
     ];
-    
+    let $match = {};
     if (price && price.length > 0) {
       data = [];
       price.map((e) => {
-        data.push({ $gte: e[0], $lte: e[1] });
+        data.push({ price: { $gte: e[0], $lte: e[1] } });
       });
-      aggQuery = [
-        ...aggQuery,
-        {
-          $match: {
-            price: { $in: data },
-          },
-        },
-      ];
+      $match = {
+        $or: data,
+      };
     }
-
-    let suggestedCampground = await Campground.aggregate(aggQuery);
-    const result =
-      suggestedCampground.length > 5
-        ? suggestedCampground
-            .slice(0, 5)
-            .sort((a, b) => b.rating - a.rating)
-            .concat(suggestedCampground.slice(5))
-        : suggestedCampground;
-    return res.status(200).json(result);
+    let response = {};
+    for (const e of allStates) {
+      $match = { ...$match, "location.state": e };
+      let suggestedCampground = await Campground.aggregate([
+        ...aggQuery,
+        { $match }
+      ]);
+      // break
+      const top5 =
+        suggestedCampground.length > 5
+          ? suggestedCampground
+              .slice(0, 5)
+              .sort((a, b) => b.overallRating - a.overallRating)
+              .concat(suggestedCampground.slice(5))
+          : suggestedCampground;
+      if (top5.length > 0) {
+        response[e] = top5;
+      }
+    }
+    return res.status(200).send(response);
   } catch (err) {
-    console.error(err);
-    console.log(err);
+    console.error(err.message);
     res.status(500).send({ message: "Internal server error" });
   }
 });
